4f80733d69a11bda587adc716897b8f9df205e91
semwett0301 <semen.mokrov@gmail.com>
2025-05-01 19:03:34 +0200

    feat: added new sampling approach

--- experiment_setup.sh
+++ experiment_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 experiment_matrix=(
-  "gpt35 vicuna-13b gpt-3.5-turbo 0 1"
-  "gpt35 vicuna-13b gpt-4 0 1"
+  "gpt35 vicuna gpt-3.5-turbo 0 1"
+  "gpt35 vicuna gpt-4 0 1"
 #  "gpt35 vicuna-13b gpt-3.5-turbo 0 3"
 #  "gpt35 vicuna-13b gpt-4 0 3"
 #  "gpt35 vicuna-13b gpt-3.5-turbo 0 6"
 #  "gpt35 vicuna-13b gpt-4 0 6"
-  "gpt35 vicuna-13b gpt-3.5-turbo 1 3"
-  "gpt35 vicuna-13b gpt-4 1 3"
+  "gpt35 vicuna gpt-3.5-turbo 1 3"
+  "gpt35 vicuna gpt-4 1 3"
 )
 for i in $(seq 1 100); do
   mkdir -p sampled_review/"${i}"
@@ -17,9 +17,9 @@ for i in $(seq 1 100); do
       row="${experiment_matrix[$i]}"
       read -r m1 m2 eval_model bpc k <<< "$row"
 
-      python3 FairEval.py \
-          -q presets/"$i".jsonl \
-          -a answer/answer_"$m1".jsonl answer/answer_"$m2".jsonl \
+      python FairEval.py \
+          -q sampled/questions/"$i".jsonl \
+          -a sampled/answer_"$m1"/"$i".jsonl sampled/answer_"$m2"/"$i".jsonl \
           -o sampled_review/"${i}"/"review_${m1}_${m2}_${eval_model}_mec${k}_bpc${bpc}.jsonl" \
           -m "$eval_model" \
           --bpc "$bpc" \
--- preset_preparation.py
+++ preset_preparation.py
@@ -2,31 +2,36 @@ import json
 import os
 import random
 
+import pandas as pd
 
-def get_json_list(file_path):
-    file_path = os.path.expanduser(file_path)
-    with open(file_path, "r") as f:
-        json_list = []
-        for line in f:
-            json_list.append(json.loads(line))
-        return json_list
 
-_AMOUNT_OF_PRESETS = 8
+
+_AMOUNT_OF_PRESETS = 4
+
+def save_items(df, id_field, id_list, save_path):
+    df[df[id_field].isin(id_list)].to_json(save_path, orient="records", lines=True)
 
 if __name__ == "__main__":
-    question_list = get_json_list('./question.jsonl')
+    question_df = pd.read_json('./question.jsonl', lines=True)
 
-    categories = set(map(lambda x: x['category'], question_list))
+    answer_alpaca_df = pd.read_json('./answer/answer_alpaca-13b.jsonl', lines=True)
+    answer_gpt_35_df = pd.read_json('./answer/answer_gpt35.jsonl', lines=True)
+    answer_gpt_4_df = pd.read_json('./answer/answer_gpt-4.jsonl', lines=True)
+    answer_vicuna_df = pd.read_json('./answer/answer_vicuna-13b.jsonl', lines=True)
+    categories = question_df['category'].unique()
 
     for i in range(_AMOUNT_OF_PRESETS):
-        sampled_list = []
+        sampled_list_ids = []
 
         for category in categories:
-            topic_questions = list(filter(lambda x: x['category'] == category, question_list))
+            topic_questions = question_df[question_df['category'] == category]
 
-            sampled_list.extend(random.sample(topic_questions, 1))
+            question_id = random.sample(topic_questions['question_id'].to_list(), 1)
+            sampled_list_ids.extend(question_id)
 
-        with open(f'./presets/{i}.jsonl', 'w') as f:
-            for item in sampled_list:
-                json.dump(item, f)
-                f.write('\n')
+        save_items(question_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/questions/{i}.jsonl')
+
+        save_items(answer_alpaca_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_alpaca/{i}.jsonl')
+        save_items(answer_gpt_35_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_gpt35/{i}.jsonl')
+        save_items(answer_gpt_4_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_gpt4/{i}.jsonl')
+        save_items(answer_vicuna_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_vicuna/{i}.jsonl')
