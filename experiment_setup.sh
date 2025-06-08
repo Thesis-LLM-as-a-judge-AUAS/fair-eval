8a40dce5bd2b31bc5f7e6c018a49888db0e4e1f2
semwett0301 <semen.mokrov@gmail.com>
2025-06-08 18:22:17 +0200

    feat: prepared the start set for lower temperature

--- experiment_setup.sh
+++ experiment_setup.sh
@@ -11,16 +11,16 @@ experiment_matrix=(
 #  "gpt35 vicuna gpt-4 1 3"
 )
 for i in $(seq 1 40); do
-  mkdir -p sampled_review/"${i}"
+  mkdir -p repeated_experiment_results/"${i}"
 
   for j in "${!experiment_matrix[@]}"; do
       row="${experiment_matrix[$j]}"
       read -r m1 m2 eval_model bpc k <<< "$row"
 
       python FairEval.py \
-          -q sampled/questions/"$j".jsonl \
-          -a sampled/answer_"$m1"/"$j".jsonl sampled/answer_"$m2"/"$j".jsonl \
-          -o repeated_experiment/"${i}"/"review_${m1}_${m2}_${eval_model}_mec${k}_bpc${bpc}.jsonl" \
+          -q repeated_sampled/questions.jsonl \
+          -a repeated_sampled/answer_"$m1".jsonl repeated_sampled/answer_"$m2".jsonl \
+          -o repeated_experiment_results/"${i}"/"review_${m1}_${m2}_${eval_model}_mec${k}_bpc${bpc}.jsonl" \
           -m "$eval_model" \
           --bpc "$bpc" \
           -k "$k"
--- preset_preparation.py
+++ preset_preparation.py
@@ -5,14 +5,17 @@ import random
 import pandas as pd
 
 
-
-_AMOUNT_OF_PRESETS = 4
-
 def save_items(df, id_field, id_list, save_path):
     df[df[id_field].isin(id_list)].to_json(save_path, orient="records", lines=True)
 
+
 if __name__ == "__main__":
+    with open('./review/review_gpt35_vicuna-13b_human.txt', 'r', encoding='utf-8') as f:
+        text_lines = f.read().splitlines()
+
     question_df = pd.read_json('./question.jsonl', lines=True)
+    question_df['ground_truth'] = text_lines
+    question_df = question_df[question_df['ground_truth'] != 'TIE']
 
     answer_alpaca_df = pd.read_json('./answer/answer_alpaca-13b.jsonl', lines=True)
     answer_gpt_35_df = pd.read_json('./answer/answer_gpt35.jsonl', lines=True)
@@ -20,18 +23,22 @@ if __name__ == "__main__":
     answer_vicuna_df = pd.read_json('./answer/answer_vicuna-13b.jsonl', lines=True)
     categories = question_df['category'].unique()
 
-    for i in range(_AMOUNT_OF_PRESETS):
-        sampled_list_ids = []
+    sampled_list_ids = []
 
-        for category in categories:
-            topic_questions = question_df[question_df['category'] == category]
+    for category in categories:
+        topic_questions = question_df[question_df['category'] == category]
 
-            question_id = random.sample(topic_questions['question_id'].to_list(), 1)
-            sampled_list_ids.extend(question_id)
+        question_id = random.sample(topic_questions['question_id'].to_list(), 1)
+        sampled_list_ids.extend(question_id)
 
-        save_items(question_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/questions/{i}.jsonl')
+    save_items(question_df, id_field='question_id', id_list=sampled_list_ids,
+               save_path=f'./repeated_sampled/questions.jsonl')
 
-        save_items(answer_alpaca_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_alpaca/{i}.jsonl')
-        save_items(answer_gpt_35_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_gpt35/{i}.jsonl')
-        save_items(answer_gpt_4_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_gpt4/{i}.jsonl')
-        save_items(answer_vicuna_df, id_field='question_id', id_list=sampled_list_ids, save_path=f'./sampled/answer_vicuna/{i}.jsonl')
+    save_items(answer_alpaca_df, id_field='question_id', id_list=sampled_list_ids,
+               save_path=f'./repeated_sampled/alpaca.jsonl')
+    save_items(answer_gpt_35_df, id_field='question_id', id_list=sampled_list_ids,
+               save_path=f'./repeated_sampled/gpt35.jsonl')
+    save_items(answer_gpt_4_df, id_field='question_id', id_list=sampled_list_ids,
+               save_path=f'./repeated_sampled/gpt4.jsonl')
+    save_items(answer_vicuna_df, id_field='question_id', id_list=sampled_list_ids,
+               save_path=f'./repeated_sampled/vicuna.jsonl')
